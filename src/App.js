b5daae828493be201e1b60aa6a7324b1247964d9
Farzana Eva <farzanaeva@Farzanas-iMac.local>
2023-02-14 23:34:14 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there! I am changing the message. </p>
+        <p>Hi there! I was change on the feature brand. </p>
         <a
           className="App-link"
           href="https://reactjs.org"
